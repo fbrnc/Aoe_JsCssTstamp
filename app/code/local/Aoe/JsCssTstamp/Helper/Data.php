0bc5f1cd8eadbfa6d16285e09cb5b35c392f9a33
Fabrizio Branca <github@fabrizio-branca.de>
2011-05-04 15:41:01 +0200

    Initial commit

---
+++ app/code/local/Aoe/JsCssTstamp/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Cache cleaner helper
+ * 
+ * @author Fabrizio Branca
+ * @since 2011-02-15
+ */
+class Aoe_JsCssTstamp_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Aoe/JsCssTstamp/Model/Package.php
@@ -0,0 +1,55 @@
+<?php
+
+class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
+	
+	/**
+	 * Overwrite original method in order to add filemtime as parameter 
+	 * 
+	 * @return string
+	 */
+    public function getMergedJsUrl($files) {
+    	$tstamp = $this->getYoungestFile($files);
+        $targetFilename = md5(implode(',', $files)) . '.' . $tstamp . '.js';
+        $targetDir = $this->_initMergerDir('js');
+        if (!$targetDir) {
+            return '';
+        }
+        if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, null, 'js')) {
+            return Mage::getBaseUrl('media') . 'js/' . $targetFilename;
+        }
+        return '';
+    }
+    	
+    /**
+	 * Overwrite original method in order to add filemtime as parameter 
+	 * 
+	 * @return string
+	 */
+     public function getMergedCssUrl($files) {
+    	$tstamp = $this->getYoungestFile($files);
+        $targetFilename = md5(implode(',', $files)) . '.' . $tstamp . '.css';
+        $targetDir = $this->_initMergerDir('css');
+        if (!$targetDir) {
+            return '';
+        }
+        if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, array($this, 'beforeMergeCss'), 'css')) {
+            return Mage::getBaseUrl('media') . 'css/' . $targetFilename; 
+        }
+        return '';
+     }
+     
+     /**
+      * Get the timestamp of the youngest file
+      * 
+      * @param array $files
+      * @return int tstamp
+      */
+     protected function getYoungestFile($files) {
+     	$tstamp = null;
+     	foreach ($files as $file) {
+     		$tstamp = is_null($tstamp) ? filemtime($file) : max($tstamp, filemtime($file));
+     	}
+     	return $tstamp;
+     }
+	
+}
