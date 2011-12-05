42e41c3e6441438898346ffff30117b962baae9c
Fabrizio Branca <github@fabrizio-branca.de>
2011-12-05 16:47:38 +0100

    Whitespace fixes, corrected phpdoc, added array check

--- app/code/local/Aoe/JsCssTstamp/Model/Package.php
+++ app/code/local/Aoe/JsCssTstamp/Model/Package.php
@@ -1,55 +1,59 @@
 <?php
 
 class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
-	
+
 	/**
-	 * Overwrite original method in order to add filemtime as parameter 
-	 * 
+	 * Overwrite original method in order to add filemtime as parameter
+	 *
+	 * @param array $files
 	 * @return string
 	 */
-    public function getMergedJsUrl($files) {
-    	$versionKey = $this->getVersionKey($files);
-        $targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.js';
-        $targetDir = $this->_initMergerDir('js');
-        if (!$targetDir) {
-            return '';
-        }
-        if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, null, 'js')) {
-            return Mage::getBaseUrl('media') . 'js/' . $targetFilename;
-        }
-        return '';
-    }
-    	
-    /**
-	 * Overwrite original method in order to add filemtime as parameter 
-	 * 
+	public function getMergedJsUrl($files) {
+		$versionKey = $this->getVersionKey($files);
+		$targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.js';
+		$targetDir = $this->_initMergerDir('js');
+		if (!$targetDir) {
+			return '';
+		}
+		if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, null, 'js')) {
+			return Mage::getBaseUrl('media') . 'js/' . $targetFilename;
+		}
+		return '';
+	}
+
+	/**
+	 * Overwrite original method in order to add filemtime as parameter
+	 *
+	 * @param array $files
 	 * @return string
 	 */
-     public function getMergedCssUrl($files) {
-    	$versionKey = $this->getVersionKey($files);
-        $targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.css';
-        $targetDir = $this->_initMergerDir('css');
-        if (!$targetDir) {
-            return '';
-        }
-        if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, array($this, 'beforeMergeCss'), 'css')) {
-            return Mage::getBaseUrl('media') . 'css/' . $targetFilename; 
-        }
-        return '';
-     }
-     
-     /**
-      * Get the timestamp of the youngest file as version key
-      * 
-      * @param array $files
-      * @return int tstamp
-      */
-     protected function getVersionKey($files) {
-     	$tstamp = null;
-     	foreach ($files as $file) {
-     		$tstamp = is_null($tstamp) ? filemtime($file) : max($tstamp, filemtime($file));
-     	}
-     	return $tstamp;
-     }
-	
+	public function getMergedCssUrl($files)	{
+		$versionKey = $this->getVersionKey($files);
+		$targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.css';
+		$targetDir = $this->_initMergerDir('css');
+		if (!$targetDir) {
+			return '';
+		}
+		if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, array($this, 'beforeMergeCss'), 'css')) {
+			return Mage::getBaseUrl('media') . 'css/' . $targetFilename;
+		}
+		return '';
+	}
+
+	/**
+	 * Get the timestamp of the youngest file as version key
+	 *
+	 * @param array $files
+	 * @return int tstamp
+	 */
+	protected function getVersionKey($files) {
+		$tstamp = null;
+		if (is_array($files)) {
+			foreach ($files as $file) {
+				$tstamp = is_null($tstamp) ? filemtime($file) : max($tstamp, filemtime($file));
+			}
+		}
+		return $tstamp;
+	}
+
 }
