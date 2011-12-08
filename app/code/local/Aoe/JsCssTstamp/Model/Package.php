9cd980168c17dcbd3e6a3634cfdd1b062d07759c
Fabrizio Branca <mail@fabrizio-branca.de>
2011-12-09 00:56:50 +0100

    Minifying js (TODO: make this optional)

--- app/code/local/Aoe/JsCssTstamp/Model/Package.php
+++ app/code/local/Aoe/JsCssTstamp/Model/Package.php
@@ -9,6 +9,10 @@ class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
 
 	const CACHEKEY = 'aoe_jscsststamp_versionkey';
 
+	protected $sumOriginalJsSize;
+	protected $sumCompressedJsSize;
+	protected $debug = false;
+
 	/**
 	 * Overwrite original method in order to add a version key
 	 *
@@ -33,7 +37,7 @@ class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
 		}
 
 		$coreHelper = Mage::helper('core'); /* @var $coreHelper Mage_Core_Helper_Data */
-		if ($coreHelper->mergeFiles($files, $path, false, null, 'js')) {
+		if ($coreHelper->mergeFiles($files, $path, false, array($this, 'beforeMergeJs'), 'js')) {
 			$mergedJsUrl = Mage::getBaseUrl('media') . 'js/' . $targetFilename;
 		}
 
@@ -44,6 +48,51 @@ class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
 		}
 
 		return $mergedJsUrl;
+	}
+
+
+	/**
+	 * Before merge JS callback function
+	 *
+	 * @param string $file
+	 * @param string $contents
+	 * @return string
+	 */
+	public function beforeMergeJs($file, $contents) {
+		require_once Mage::getBaseDir('lib').'/aoe_jscsststamp/JSMin.php';
+		if ($this->debug) {
+			$lengthBefore = strlen($contents);
+			$this->sumOriginalJsSize += $lengthBefore;
+		}
+
+		$contents = JSMin::minify($contents);
+
+		if ($this->debug) {
+			$lengthAfter = strlen($contents);
+			$this->sumCompressedJsSize += $lengthAfter;
+			$saved = $lengthBefore - $lengthAfter;
+			$percent = 100 - round(100 * $lengthAfter / $lengthBefore);
+			$savedSoFar = $this->sumOriginalJsSize - $this->sumCompressedJsSize;
+			$percentSoFar = 100 - round(100 * $this->sumCompressedJsSize / $this->sumOriginalJsSize);
+			Mage::log(sprintf("Compressing %s: %s -> %s, Saved: %s (%s %%) | Total: %s -> %s, Saved: %s (%s %%)",
+				substr($file, -20),
+				$this->formatSize($lengthBefore),
+				$this->formatSize($lengthAfter),
+				$this->formatSize($saved),
+				$percent,
+				$this->formatSize($this->sumOriginalJsSize),
+				$this->formatSize($this->sumCompressedJsSize),
+				$this->formatSize($savedSoFar),
+				$percentSoFar
+			));
+		}
+		return $contents;
+	}
+
+	protected function formatSize($size) {
+		$sizes = array(" Bytes", " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
+		if ($size == 0) { return('n/a'); } else {
+		return (round($size/pow(1024, ($i = floor(log($size, 1024)))), 2) . $sizes[$i]); }
 	}
 
 	/**
