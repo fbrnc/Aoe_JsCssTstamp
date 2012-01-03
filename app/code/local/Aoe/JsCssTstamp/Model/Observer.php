335435c3f209ce5801e31c53ac32d8dc0dc9d4d1
Fabrizio Branca <github@fabrizio-branca.de>
2012-01-03 18:37:46 +0100

    Cleaning databases when flushing the corresponding cache

--- app/code/local/Aoe/JsCssTstamp/Model/Observer.php
+++ app/code/local/Aoe/JsCssTstamp/Model/Observer.php
@@ -8,8 +8,13 @@ class Aoe_JsCssTstamp_Model_Observer {
 	 * @return void
 	 */
 	public function clean_media_cache_after() {
-		// TODO: clean files from databasem
 		Mage::app()->removeCache(Aoe_JsCssTstamp_Model_Package::CACHEKEY);
+
+		// clean files from database
+		$dbStorage = Mage::helper('core/file_storage_database')->getStorageDatabaseModel(); /* @var $dbStorage Mage_Core_Model_File_Storage_Database */
+		$dbStorage->getDirectoryModel()
+			->deleteDirectory('js')
+			->deleteDirectory('css');
 	}
 
 }
