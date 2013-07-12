a6f167c47c4a178861c664a95e7c63dd232c146b
Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
2013-07-12 15:31:03 +0200

    MAGE-437: Prepare for Cloud: Make CSS and JS ouside of media - refactored Aoe_JsCssTstamp_Model_Package to allow extend it in Aoe_MergedJsCssCdn for cdn support

--- app/code/local/Aoe/JsCssTstamp/Model/Package.php
+++ app/code/local/Aoe/JsCssTstamp/Model/Package.php
@@ -5,251 +5,254 @@
  *
  * @author Fabrizio Branca
  */
-class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
+class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package
+{
+    const CACHEKEY = 'aoe_jscsststamp_versionkey';
 
-	const CACHEKEY = 'aoe_jscsststamp_versionkey';
+    protected $cssProtocolRelativeUris;
+    protected $jsProtocolRelativeUris;
+    protected $storeCssInDb;
+    protected $storeJsInDb;
+    protected $dbStorage;
+    protected $addTstampToAssets;
+    protected $storeMinifiedCssFolder;
+    protected $storeMinifiedJsFolder;
 
-	protected $cssProtocolRelativeUris;
-	protected $jsProtocolRelativeUris;
-	protected $storeCssInDb;
-	protected $storeJsInDb;
-	protected $dbStorage;
-	protected $addTstampToAssets;
-	protected $storeMinifiedCssFolder;
-	protected $storeMinifiedJsFolder;
+    /**
+     * Constructor
+     *
+     * Hint: Parent class is a plain php class not extending anything. So don't try to move this content to _construct()
+     */
+    public function __construct()
+    {
+        $this->cssProtocolRelativeUris = Mage::getStoreConfig('dev/css/protocolRelativeUris');
+        $this->jsProtocolRelativeUris  = Mage::getStoreConfig('dev/js/protocolRelativeUris');
+        $this->storeCssInDb            = Mage::getStoreConfig('dev/css/storeInDb');
+        $this->storeJsInDb             = Mage::getStoreConfig('dev/js/storeInDb');
+        $this->addTstampToAssets       = Mage::getStoreConfig('dev/css/addTstampToAssets');
+        $this->storeMinifiedCssFolder  = rtrim(Mage::getBaseDir(), DS)
+            . DS . trim(Mage::getStoreConfig('dev/css/storeMinifiedCssFolder'), DS);
+        $this->storeMinifiedJsFolder   = rtrim(Mage::getBaseDir(), DS)
+            . DS . trim(Mage::getStoreConfig('dev/js/storeMinifiedJsFolder'), DS);
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * Hint: Parent class is a plain php class not extending anything. So don't try to move this content to _construct()
-	 */
-	public function __construct() {
-		$this->cssProtocolRelativeUris = Mage::getStoreConfig('dev/css/protocolRelativeUris');
-		$this->jsProtocolRelativeUris = Mage::getStoreConfig('dev/js/protocolRelativeUris');
-		$this->storeCssInDb = Mage::getStoreConfig('dev/css/storeInDb');
-		$this->storeJsInDb = Mage::getStoreConfig('dev/js/storeInDb');
-		$this->addTstampToAssets = Mage::getStoreConfig('dev/css/addTstampToAssets');
-		$this->storeMinifiedCssFolder = rtrim(Mage::getBaseDir(), DS)
-			. DS . trim(Mage::getStoreConfig('dev/css/storeMinifiedCssFolder'), DS);
-		$this->storeMinifiedJsFolder = rtrim(Mage::getBaseDir(), DS)
-			. DS . trim(Mage::getStoreConfig('dev/js/storeMinifiedJsFolder'), DS);
-	}
+    /**
+     * Get db storage
+     *
+     * @return Mage_Core_Model_File_Storage_Database
+     */
+    protected function getDbStorage()
+    {
+        if (is_null($this->dbStorage)) {
+            $this->dbStorage = Mage::helper('core/file_storage_database')->getStorageDatabaseModel();
+        }
 
-	/**
-	 * Get db storage
-	 *
-	 * @return Mage_Core_Model_File_Storage_Database
-	 */
-	protected function getDbStorage() {
-		if (is_null($this->dbStorage)) {
-			$this->dbStorage = Mage::helper('core/file_storage_database')->getStorageDatabaseModel();
-		}
-		return $this->dbStorage;
-	}
+        return $this->dbStorage;
+    }
 
-	/**
-	 * Overwrite original method in order to add a version key
-	 *
-	 * @param array $files
-	 * @return string
-	 */
-	public function getMergedJsUrl($files) {
+    /**
+     * Overwrite original method in order to add a version key
+     *
+     * @param array $files
+     * @return string
+     */
+    public function getMergedJsUrl($files)
+    {
+        $versionKey     = $this->getVersionKey();
+        $targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.js';
+        $targetDir      = $this->_initMergerDir('js');
+        if (!$targetDir) {
+            return '';
+        }
 
-		$versionKey = $this->getVersionKey();
-		$targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.js';
-		$targetDir = $this->_initMergerDir('js');
-		if (!$targetDir) {
-			return '';
-		}
+        $mergedJsUrl = $this->generateMergedUrl('js', $files, $targetDir, $targetFilename);
+        if ($this->jsProtocolRelativeUris) {
+            $mergedJsUrl = $this->convertToProtocolRelativeUri($mergedJsUrl);
+        }
 
-		$path = $targetDir . DS . $targetFilename;
+        return $mergedJsUrl;
+    }
 
-		// relative path
-		$relativePath = str_replace(Mage::getBaseDir('media'), '', $path);
-		$relativePath = ltrim($relativePath, DS);
+    /**
+     * Before merge JS callback function
+     *
+     * @param string $file
+     * @param string $contents
+     * @return string
+     */
+    public function beforeMergeJs($file, $contents)
+    {
+        $minContent = $this->useMinifiedVersion($file);
+        if ($minContent !== false) {
+            $contents = $minContent;
+        }
 
-		/* @var $dbStorage Mage_Core_Model_File_Storage_Database */
-		$dbStorage = $this->getDbStorage();
+        $contents = "\n\n/* FILE: " . basename($file) . " */\n" . $contents;
 
-		$mergedJsUrl = Mage::getBaseUrl('media') . 'js' . DS . $targetFilename;
+        return $contents;
+    }
 
-		if (!$dbStorage->fileExists($relativePath)) {
-			$coreHelper = Mage::helper('core');
-			/* @var $coreHelper Mage_Core_Helper_Data */
-			if (!$coreHelper->mergeFiles($files, $path, FALSE, array($this, 'beforeMergeJs'), 'js')) {
-				Mage::throwException('Error while merging js files to path ' . $relativePath);
-			}
-			$dbStorage->saveFile($relativePath);
-		}
+    /**
+     * Before merge CSS callback function
+     *
+     * @param string $file
+     * @param string $contents
+     * @return string
+     */
+    public function beforeMergeCss($file, $contents)
+    {
+        $minContent = $this->useMinifiedVersion($file);
+        if ($minContent !== false) {
+            $contents = $minContent;
+        }
 
-		if ($this->jsProtocolRelativeUris) {
-			$mergedJsUrl = $this->convertToProtocolRelativeUri($mergedJsUrl);
-		}
+        $contents = "\n\n/* FILE: " . basename($file) . " */\n" . $contents;
 
-		return $mergedJsUrl;
-	}
+        return parent::beforeMergeCss($file, $contents);
+    }
 
-	/**
-	 * Before merge JS callback function
-	 *
-	 * @param string $file
-	 * @param string $contents
-	 * @return string
-	 */
-	public function beforeMergeJs($file, $contents) {
-		$minContent = $this->useMinifiedVersion($file);
-		if ($minContent !== FALSE) {
-			$contents = $minContent;
-		}
+    /**
+     * Checks if minified version of the given file exist. And if returns its content
+     *
+     * @param string $file
+     * @return string|bool the content of the file else false
+     */
+    protected function useMinifiedVersion($file)
+    {
+        $parts = pathinfo($file);
+        // Add .min to the extension of the original filename
+        $minFile = $parts['dirname'] . DS . $parts['filename'] . '.min.' . $parts['extension'];
 
-		$contents = "\n\n/* FILE: " . basename($file) . " */\n" . $contents;
-		return $contents;
-	}
+        if (file_exists($minFile)) {
+            // return the content of the min file @see Mage_Core_Helper_Data -> mergeFiles()
+            return file_get_contents($minFile) . "\n";
+        } else {
+            $pathRelativeToBase = str_replace(Mage::getBaseDir(), '', $parts['dirname']);
+            $pathRelativeToBase = ltrim($pathRelativeToBase, DS);
 
-	/**
-	 * Before merge CSS callback function
-	 *
-	 * @param string $file
-	 * @param string $contents
-	 * @return string
-	 */
-	public function beforeMergeCss($file, $contents) {
-		$minContent = $this->useMinifiedVersion($file);
-		if ($minContent !== FALSE) {
-			$contents = $minContent;
-		}
+            switch ($parts['extension']) {
+                case 'js':
+                    $minFile = $this->storeMinifiedJsFolder . DS . $pathRelativeToBase
+                        . DS . $parts['filename'] . '.min.' . $parts['extension'];
+                    break;
+                case 'css':
+                default:
+                    $minFile = $this->storeMinifiedCssFolder . DS . $pathRelativeToBase
+                        . DS . $parts['filename'] . '.min.' . $parts['extension'];
+                    break;
+            }
 
-		$contents = "\n\n/* FILE: " . basename($file) . " */\n" . $contents;
-		return parent::beforeMergeCss($file, $contents);
-	}
+            if (file_exists($minFile)) {
+                // return the content of the min file @see Mage_Core_Helper_Data -> mergeFiles()
+                return file_get_contents($minFile) . "\n";
+            }
+        }
 
-	/**
-	 * Checks if minified version of the given file exist. And if returns its content
-	 *
-	 * @param string $file
-	 * @return string|bool the content of the file else false
-	 */
-	protected function useMinifiedVersion($file) {
-		$parts = pathinfo($file);
-		// Add .min to the extension of the original filename
-		$minFile = $parts['dirname'] . DS . $parts['filename'] . '.min.' . $parts['extension'];
+        return false;
+    }
 
-		if (file_exists($minFile)) {
-			// return the content of the min file @see Mage_Core_Helper_Data -> mergeFiles()
-			return file_get_contents($minFile) . "\n";
-		} else {
-			$pathRelativeToBase = str_replace(Mage::getBaseDir(), '', $parts['dirname']);
-			$pathRelativeToBase = ltrim($pathRelativeToBase, DS);
+    /**
+     * Overwrite original method in order to add a version key
+     *
+     * @param array $files
+     * @return string
+     */
+    public function getMergedCssUrl($files)
+    {
+        $versionKey     = $this->getVersionKey();
+        $targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.css';
+        $targetDir      = $this->_initMergerDir('css');
+        if (!$targetDir) {
+            return '';
+        }
 
-			switch ($parts['extension']) {
-				case 'js':
-					$minFile = $this->storeMinifiedJsFolder . DS . $pathRelativeToBase
-						. DS . $parts['filename'] . '.min.' . $parts['extension'];
-					break;
-				case 'css':
-				default:
-					$minFile = $this->storeMinifiedCssFolder . DS . $pathRelativeToBase
-						. DS . $parts['filename'] . '.min.' . $parts['extension'];
-				break;
-			}
+        $mergedCssUrl = $this->generateMergedUrl('css', $files, $targetDir, $targetFilename);
+        if ($this->cssProtocolRelativeUris) {
+            $mergedCssUrl = $this->convertToProtocolRelativeUri($mergedCssUrl);
+        }
 
-			if (file_exists($minFile)) {
-				// return the content of the min file @see Mage_Core_Helper_Data -> mergeFiles()
-				return file_get_contents($minFile) . "\n";
-			}
-		}
+        return $mergedCssUrl;
+    }
 
-		return FALSE;
-	}
+    /**
+     * Generate url for merged file of given $type
+     *
+     * @param string $type
+     * @param array $files
+     * @param string $targetDir
+     * @param string $targetFilename
+     * @return string
+     */
+    protected function generateMergedUrl($type, array $files, $targetDir, $targetFilename)
+    {
+        $path = $targetDir . DS . $targetFilename;
 
-	/**
-	 * Overwrite original method in order to add a version key
-	 *
-	 * @param array $files
-	 * @return string
-	 */
-	public function getMergedCssUrl($files) {
+        // relative path
+        $relativePath = ltrim(str_replace(Mage::getBaseDir('media'), '', $path), DS);
+        /* @var $dbStorage Mage_Core_Model_File_Storage_Database */
+        $dbStorage = $this->getDbStorage();
+        if (!$dbStorage->fileExists($relativePath)) {
+            $coreHelper = Mage::helper('core');
+            /* @var $coreHelper Mage_Core_Helper_Data */
+            if (!$coreHelper->mergeFiles($files, $path, false, array($this, 'beforeMerge' . ucfirst($type)), $type)) {
+                Mage::throwException("Error while merging {$type} files to path: " . $relativePath);
+            }
+            $dbStorage->saveFile($relativePath);
+        }
 
-		$versionKey = $this->getVersionKey();
-		$targetFilename = md5(implode(',', $files)) . '.' . $versionKey . '.css';
-		$targetDir = $this->_initMergerDir('css');
-		if (!$targetDir) {
-			return '';
-		}
+        return Mage::getBaseUrl('media') . $type . DS . $targetFilename;
+    }
 
-		$path = $targetDir . DS . $targetFilename;
+    /**
+     * Get a cached timestamp as version key
+     *
+     * @return int timestamp
+     */
+    protected function getVersionKey()
+    {
+        $timestamp = Mage::app()->loadCache(self::CACHEKEY);
+        if (empty($timestamp)) {
+            $timestamp = time();
+            Mage::app()->saveCache($timestamp, self::CACHEKEY, array(), null);
+        }
 
-		// relative path
-		$relativePath = str_replace(Mage::getBaseDir('media'), '', $path);
-		$relativePath = ltrim($relativePath, DS);
+        return $timestamp;
+    }
 
-		$dbStorage = $this->getDbStorage();
-		/* @var $dbStorage Mage_Core_Model_File_Storage_Database */
+    /**
+     * Convert uri to protocol independent uri
+     * E.g. http://example.com -> //example.com
+     *
+     * @param string $uri
+     * @return string
+     */
+    protected function convertToProtocolRelativeUri($uri)
+    {
+        return preg_replace('/^https?:/i', '', $uri);
+    }
 
-		$mergedCssUrl = Mage::getBaseUrl('media') . 'css' . DS . $targetFilename;
+    /**
+     * Convert uri to protocol independent uri
+     * E.g. http://example.com -> //example.com
+     *
+     * @param string $uri
+     * @return string
+     */
+    protected function _prepareUrl($uri)
+    {
+        $uri = parent::_prepareUrl($uri);
+        if ($this->cssProtocolRelativeUris) {
+            $uri = $this->convertToProtocolRelativeUri($uri);
+        }
 
-		if (!$dbStorage->fileExists($relativePath)) {
-			$coreHelper = Mage::helper('core');
-			/* @var $coreHelper Mage_Core_Helper_Data */
-			if (!$coreHelper->mergeFiles($files, $path, FALSE, array($this, 'beforeMergeCss'), 'css')) {
-				Mage::throwException('Error while merging css files to path: ' . $relativePath);
-			}
-			$dbStorage->saveFile($relativePath);
-		}
+        if ($this->addTstampToAssets) {
+            Mage::log('Aoe_JsCssTsamp: ' . $uri);
+            $matches = array();
+            if (preg_match('/(.*)\.(gif|png|jpg)$/i', $uri, $matches)) {
+                $uri = $matches[1] . '.' . $this->getVersionKey() . '.' . $matches[2];
+            }
+        }
 
-		if ($this->cssProtocolRelativeUris) {
-			$mergedCssUrl = $this->convertToProtocolRelativeUri($mergedCssUrl);
-		}
-
-		return $mergedCssUrl;
-	}
-
-	/**
-	 * Get a cached timestamp as version key
-	 *
-	 * @param array $files
-	 * @return int tstamp
-	 */
-	protected function getVersionKey() {
-		$tstamp = Mage::app()->loadCache(self::CACHEKEY);
-		if (empty($tstamp)) {
-			$tstamp = time();
-			Mage::app()->saveCache($tstamp, self::CACHEKEY, array(), NULL);
-		}
-		return $tstamp;
-	}
-
-	/**
-	 * Convert uri to protocol independent uri
-	 * E.g. http://example.com -> //example.com
-	 *
-	 * @param string $uri
-	 * @return string
-	 */
-	protected function convertToProtocolRelativeUri($uri) {
-		return preg_replace('/^https?:/i', '', $uri);
-	}
-
-	/**
-	 * Convert uri to protocol independent uri
-	 * E.g. http://example.com -> //example.com
-	 *
-	 * @param string $uri
-	 * @return string
-	 */
-	protected function _prepareUrl($uri) {
-		$uri = parent::_prepareUrl($uri);
-		if ($this->cssProtocolRelativeUris) {
-			$uri = $this->convertToProtocolRelativeUri($uri);
-		}
-
-		if ($this->addTstampToAssets) {
-			Mage::log('Aoe_JsCssTsamp: ' . $uri);
-			$matches = array();
-			if (preg_match('/(.*)\.(gif|png|jpg)$/i', $uri, $matches)) {
-				$uri = $matches[1] . '.' . $this->getVersionKey() . '.' . $matches[2];
-			}
-		}
-		return $uri;
-	}
+        return $uri;
+    }
 }
