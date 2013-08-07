c3a75da9ed3bccaa1375d333cfd0bf9eb806227b
Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
2013-08-07 11:26:44 +0200

    fixed bug in magento when $mergeCallback returns no url - file names instead of file urls should be used

--- app/code/local/Aoe/JsCssTstamp/Block/Head.php
+++ app/code/local/Aoe/JsCssTstamp/Block/Head.php
@@ -98,4 +98,73 @@ class Aoe_JsCssTstamp_Block_Head extends Mage_Page_Block_Html_Head {
 		return $this;
 	}
 
+	/**
+	 * Merge static and skin files of the same format into 1 set of HEAD directives or even into 1 directive
+	 *
+	 * Will attempt to merge into 1 directive, if merging callback is provided. In this case it will generate
+	 * filenames, rather than render urls.
+	 * The merger callback is responsible for checking whether files exist, merging them and giving result URL
+	 *
+	 * @param string $format - HTML element format for sprintf('<element src="%s"%s />', $src, $params)
+	 * @param array $staticItems - array of relative names of static items to be grabbed from js/ folder
+	 * @param array $skinItems - array of relative names of skin items to be found in skins according to design config
+	 * @param callback $mergeCallback
+	 * @return string
+	 */
+	protected function &_prepareStaticAndSkinElements($format, array $staticItems, array $skinItems,
+		$mergeCallback = null
+	) {
+		$designPackage = Mage::getDesign();
+		$baseJsUrl     = Mage::getBaseUrl('js');
+		$items         = array();
+		if ($mergeCallback && !is_callable($mergeCallback)) {
+			$mergeCallback = null;
+		}
+
+		// get static files from the js folder, no need in lookups
+		foreach ($staticItems as $params => $rows) {
+			$items[$params] = array(
+				'files' => array(),
+				'urls' => array(),
+			);
+			foreach ($rows as $name) {
+				$items[$params]['files'][] = Mage::getBaseDir() . DS . 'js' . DS . $name;
+				$items[$params]['urls'][]  = $baseJsUrl . $name;
+			}
+		}
+
+		// lookup each file basing on current theme configuration
+		foreach ($skinItems as $params => $rows) {
+			$items[$params] = array(
+				'files' => array(),
+				'urls' => array(),
+			);
+			foreach ($rows as $name) {
+				$items[$params]['files'][] = $designPackage->getFilename($name, array('_type' => 'skin'));
+				$items[$params]['urls'][]  = $designPackage->getSkinUrl($name, array());
+			}
+		}
+
+		$html = '';
+		foreach ($items as $params => $rows) {
+			// attempt to merge
+			$mergedUrl = false;
+			if ($mergeCallback) {
+				$mergedUrl = call_user_func($mergeCallback, $rows['files']);
+			}
+			// render elements
+			$params = trim($params);
+			$params = $params ? ' ' . $params : '';
+			if ($mergedUrl) {
+				$html .= sprintf($format, $mergedUrl, $params);
+			} else {
+				foreach ($rows['urls'] as $src) {
+					$html .= sprintf($format, $src, $params);
+				}
+			}
+		}
+
+		return $html;
+	}
+
 }
